ecf4554783d9261a511f5a5d3011e5e512fdb062
somepingu <131262091+somepingu@users.noreply.github.com>
2026-01-28 15:59:55 +0100

    Yahtzee struggles

---
+++ dobbelstenen/src/components/ScoreTable.vue
@@ -0,0 +1,98 @@
+
+// receives dice from App.vue and calculates displays the score table
+// change dice to key value structure before calculating scores
+<script setup>
+
+</script>
+
+<template>
+    <h3>Score Table</h3>
+    <table>
+    <thead>
+            <tr>
+                <td>Deel 1</td>
+                <td>1e spel</td>
+                <td>2e spel</td>
+                <td>3e spel</td>
+                <td>4e spel</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Enen</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Tweeen</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Drieen</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Vieren</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Vijfen</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Zessen</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Bonus</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Totaal deel 1</td>
+                <td>0</td>
+            </tr>
+        </tbody>
+        <tbody>
+            <tr>
+                <td>Deel 2</td>
+            </tr>
+            <tr>
+                <td>Three of a kind</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Carre</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Full house</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Kleine straat</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Grote straat</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Yahtzee</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Chance</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Totaal deel 2</td>
+                <td>0</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <td>Totale score</td>
+                <td>0</td>
+            </tr>
+        </tfoot>
+    </table>
+</template>
---
+++ dobbelstenen/src/components/ThrownDice.vue
@@ -0,0 +1,30 @@
+
+// displays the thrown dice as well as the button to throw. Also the logic should be in here (remember that dice array in in App.vue)
+<script setup>
+const NUMBER_OF_ROLLS = 5;
+import { ref } from 'vue';
+
+const props = defineProps({
+    dice: {
+        type: Array,
+        required: true
+    }
+})
+
+const emit = defineEmits(['updateDice']);
+
+const rollDice = () => {
+    const newDice = [];
+    for (let i = 0; i < NUMBER_OF_ROLLS; i++) {
+        const roll = (Math.floor(Math.random() * 6) + 1);
+        newDice.push(roll);
+    }
+    emit('updateDice', newDice);
+}
+
+</script>
+
+<template>
+    <h3>Thrown Dice: <li v-for="(die, index) in props.dice" :key="index"> {{ die }}</li></h3>
+    <button @click="rollDice">Gooi dobbelstenen!</button>
+</template>
