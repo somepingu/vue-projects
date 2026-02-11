0015d760bf284c45b56a0d43390e9528bdeb2926
somepingu <131262091+somepingu@users.noreply.github.com>
2026-02-11 14:40:34 +0100

    revert

--- boodschappenlijst/src/domains/groceries/routes.js
+++ boodschappenlijst/src/domains/groceries/routes.js
@@ -2,10 +2,8 @@ import OverviewView from './pages/Overview.vue'
 import CreateView from './pages/Create.vue'
 import EditView from './pages/Edit.vue'
 
-const routes = [
+export const groceryRoutes = [
   { path: '/', component: OverviewView },
   { path: '/create', component: CreateView },
   { path: '/edit', component: EditView },
 ]
-
-export default routes
--- boodschappenlijst/src/main.js
+++ boodschappenlijst/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from './domains/groceries/index'
+import router from './router/index'
 
 createApp(App).use(router).mount('#app')
